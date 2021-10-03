0f2687be6463efc3048f4c575c81c5ea4a0a7859
dbmartinez <dbmartinez@me.com>
2021-10-03 03:56:57 -0700

    initial commit

---
+++ program 1.cpp
@@ -0,0 +1,39 @@
+// program 1
+
+#include <iostream>
+using namespace std;
+
+void display();
+int calc(int, int);
+
+void display()
+{
+	int x, y;
+
+	cout << "\nEnter in 2 numbers and I'll add them together\n";
+	cout << "Enter in first number: ";
+	cin >> x;
+
+	cout << "\nEnter in second number: ";
+	cin >> y;
+
+	calc(x, y);
+}
+
+int calc(int a, int b)
+{
+	int z;
+
+	z = a+b;
+
+	cout << "\n\nAnswer is : " << z;
+
+	return z;
+}
+
+int main()
+{
+	display();	
+
+	return 0;
+}
