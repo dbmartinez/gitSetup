d8994f643324b72176f3d68dcd3a1b9ca12267fc
dbmartinez <dbmartinez@me.com>
2021-10-03 16:58:00 -0700

    Updated more comments to code

--- program 1.cpp
+++ program 1.cpp
@@ -11,25 +11,32 @@ int calc(int, int);
 // display function
 void display()
 {
+	// variables
 	int x, y;
 
+	// display prompt/obtain user input1
 	cout << "\nEnter in 2 numbers and I'll add them together\n";
 	cout << "Enter in first number: ";
 	cin >> x;
 
+	// obtain user input2
 	cout << "\nEnter in second number: ";
 	cin >> y;
 
+	// send user input to cacl() function
 	calc(x, y);
 }
 
 // calculation function
 int calc(int a, int b)
 {
+	// variables
 	int z;
 
+	// add both integers
 	z = a+b;
 
+	// show calculation
 	cout << "\n\nAnswer is : " << z;
 
 	return z;
@@ -38,6 +45,7 @@ int calc(int a, int b)
 // main driver
 int main()
 {
+	// call display function
 	display();	
 
 	return 0;
