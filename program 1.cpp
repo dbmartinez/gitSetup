97df2d95a94dcb0a981f57173e997fe29e028964
Daniel Martinez <57377837+dbmartinez@users.noreply.github.com>
2021-10-03 16:38:09 -0700

    Second commit
    added comments to program

--- program 1.cpp
+++ program 1.cpp
@@ -1,13 +1,14 @@
 // program 1
 
-
 // Header files
 #include <iostream>
 using namespace std;
 
+// Function prototypes
 void display();
 int calc(int, int);
 
+// display function
 void display()
 {
 	int x, y;
@@ -22,6 +23,7 @@ void display()
 	calc(x, y);
 }
 
+// calculation function
 int calc(int a, int b)
 {
 	int z;
@@ -33,6 +35,7 @@ int calc(int a, int b)
 	return z;
 }
 
+// main driver
 int main()
 {
 	display();	
